0b7745a5d93b3cdd8a13d5d0fc09e30a7bee34f6
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2024-03-19 14:19:30 +0300

    added main handler, static index.html and password hasher

---
+++ handlers/main_handler.go
@@ -0,0 +1,14 @@
+package handlers
+
+import "github.com/labstack/echo/v4"
+
+
+type StaticHandler struct{}
+
+func NewStaticHandler() *StaticHandler {
+    return &StaticHandler{}
+}
+
+func (h *StaticHandler) IndexHTML(c echo.Context) error {
+    return c.File("templates/index.html")
+}
---
+++ helpers/password.go
@@ -0,0 +1,24 @@
+package helpers
+
+import (
+    "crypto/rand"
+    "crypto/sha256"
+    "encoding/hex"
+)
+
+
+func GenerateSalt() (string, error) {
+    salt := make([]byte, 32)
+    _, err := rand.Read(salt)
+    if err != nil {
+        return "", err
+    }
+    return hex.EncodeToString(salt), nil
+}
+
+func HashPassword(password, salt string) string {
+    hash := sha256.New()
+    hash.Write([]byte(password + salt))
+    hashedPassword := hex.EncodeToString(hash.Sum(nil))
+    return hashedPassword
+}
