32d5097d666c6e92a3fca9e569bfa08900959d85
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2024-03-22 19:25:27 +0300

    edited methods on authentication side and added verification on server side

--- handlers/auth.go
+++ handlers/auth.go
@@ -5,6 +5,7 @@ import (
 	"berjcode/dependency/database"
 	"berjcode/dependency/helpers"
 	"berjcode/dependency/models"
+	"errors"
 	"net/http"
 	"time"
 
@@ -23,6 +24,10 @@ func Login(c echo.Context) error {
 		return echo.ErrBadRequest
 	}
 
+	if err := validateLoginForm(loginForm); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+
 	var dbUser models.User
 	if err := db.Where("user_name = ? OR email = ?", loginForm.UsernameAndEmail, loginForm.UsernameAndEmail).First(&dbUser).Error; err != nil {
 		return echo.ErrBadRequest
@@ -36,4 +41,20 @@ func Login(c echo.Context) error {
 	http.SetCookie(c.Response(), cookie)
 
 	return c.JSON(http.StatusOK, map[string]string{"message": constant.SuccessLogin})
+}
+
+func validateLoginForm(loginModel models.UserLogin) error {
+	if loginModel.Password == "" || loginModel.UsernameAndEmail == "" {
+		return errors.New("bu alanlar zorunludur")
+	}
+
+	if len(loginModel.Password) > 2 && len(loginModel.Password) < 20 {
+		return errors.New("bu alanlar 2  ile 20 karakterden oluşmalı")
+	}
+
+	if len(loginModel.UsernameAndEmail) > 2 && len(loginModel.UsernameAndEmail) < 30 {
+		return errors.New("bu alanlar 2 karakterden büyük olmalı")
+	}
+
+	return nil
 }
