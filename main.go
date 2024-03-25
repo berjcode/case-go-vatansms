3cc1177bb5b5c07650d8276fa7979e02144120a7
Abdullah Balıkçı <abdullah.balikci20@ogr.atauni.edu.tr>
2024-03-25 04:11:50 +0300

    main.go file cleaned

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	staticHandler := handlers.NewStaticHandler()
 	database.Migrate(db)
 	e.Static("/static", "static")
 	// Login
@@ -35,7 +34,6 @@ func main() {
 	e.POST("/v1/users", handlers.CreateUser)
 
 	// Lesson
-	e.PUT("/v1/lessons", staticHandler.LessonDetailPageHtml, mymiddleware.AuthMiddleware)
 	e.PUT("/v1/lessons", handlers.UpdateLesson, mymiddleware.AuthMiddleware)
 	e.POST("/v1/lessons", handlers.CreateUserLesson, mymiddleware.AuthMiddleware)
 	e.GET("/v1/lessons/:id", handlers.GetLessonById, mymiddleware.AuthMiddleware)
@@ -53,11 +51,5 @@ func main() {
 	e.GET("/v1/plans/:id", handlers.GetPlanById, mymiddleware.AuthMiddleware)
 	e.GET("/v1/plans/:userid", handlers.GetPlanDetails, mymiddleware.AuthMiddleware)
 
-	// e.GET("/", staticHandler.IndexHTML)
-	// e.GET("/login", staticHandler.LoginPageHtml)
-	// e.GET("/plan", staticHandler.PlanPageHTML, mymiddleware.AuthMiddleware)
-	// e.GET("/register", staticHandler.RegisterHTML)
-	// e.GET("/lesson", staticHandler.LessonPageHtml, mymiddleware.AuthMiddleware)
-	// e.GET("/userdetail", staticHandler.UserDetailPage, mymiddleware.AuthMiddleware)
 	e.Logger.Fatal(e.Start(":8080"))
 }
